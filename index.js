fe51e266dd344e29ad6595ee9cd1e8fe56435c82
Damien CAYE <caye.damien@gmail.com>
2024-05-07 12:31:57 +0200

    Add db connection and .env.sample

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require('express');
 const sequelize = require("sequelize");
 const app = express();
 const port = 3003;
+
+const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: "db",
+    dialect: "mysql"
+  });

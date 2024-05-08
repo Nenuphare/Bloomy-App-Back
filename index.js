d018a1df8f37f7f0db8cc58d27d66acd8cabb659
Nenuphare <caye.damien@gmail.com>
2024-05-08 21:16:09 +0200

    Add app.use to index.js

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ const db = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
     host: "db",
     dialect: "mysql"
   });
+
+// Test de la connexion à la base de données
+db.authenticate()
+.then(() => {
+  console.log("Connecté à la base de données MySQL!");
+})
+.catch(err => {
+  console.error("Impossible de se connecter à la base de données:", err);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+// Démarrage du serveur
+app.listen(port, () => {
+  console.log(`L'application écoute sur le port ${port}`);
+});

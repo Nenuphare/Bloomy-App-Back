21af41f886c34749eb2b62dec64cbb841cd86595
Alexis <alexishadjian@gmail.com>
2024-06-09 18:51:24 +0200

    build: remove mariadb, cleaning

--- models/index.js
+++ models/index.js
@@ -1,13 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
-console.log(process.env.DB_NAME);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_PORT);
-console.log(process.env.PORT);
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: 'mysql',
   host: process.env.DB_HOST,
